c72f11427e31ad9bbaddbe643193294aa9f82c46
SeongJeon <hsoojy_@naver.com>
2019-03-11 01:35:41 +0900

    no message

--- static/js/common.js
+++ static/js/common.js
@@ -121,22 +121,6 @@ $(document).on("click", "body", function(e){
 })
 
 
-$(document).on("click", ".js-btn-pause", function(e){
-	var slickName = $(this).attr("data-slick"); //, $this = $(el);
-	console.log(slickName);
-	// if($this.hasClass('js-pause')){
-	// 	$('span', $this).text("Pause");
-	// 	$(slickName).slick('slickPlay');
-	// }else{
-	// 	$('span', $this).text("Play");
-		$("."+slickName).slick('slickPause');
-	// }
-
-	// $this.toggleClass("js-pause");
-	return false;
-})
-
-
 
 
 //-------------------------------------------------------------------------
